be9004da615cd265148f4385785abaa15f320f12
EAISHWARYA <62337899+EAISHWARYA@users.noreply.github.com>
2020-09-28 19:26:26 +0530

    Add files via upload

---
+++ screens/BookDonateScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, FlatList,TouchableOpacity } from 'react-native';
+
+
+export default class BookDonateScreen extends Component{
+ render(){
+   return(
+<Text request screen/>
+   )
+ }
+}
---
+++ screens/BookRequestScreen.js
@@ -0,0 +1,23 @@
+import React,{Component} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  StyleSheet,
+  TouchableOpacity,
+  Alert} from 'react-native';
+
+
+export default class BookRequestScreen extends Component{
+  
+ 
+  render(){
+    return(
+<Text donate screen/>
+
+
+    )
+  }
+}
+        
